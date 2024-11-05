8a2d60d07e79024a08ba60fa05309342c97fca15
Pawan <pawanchaukiyal@gmail.com>
2024-11-05 14:49:04 +0530

    component_call--pc

--- pages/homepage/home.js
+++ pages/homepage/home.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', 
+    function jkl() {
     fetch('../../components/Navbar/Navbar.html')
         .then(response => response.text())
         .then(data => {
         
             document.querySelector('.nav-item').innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error loading navbar:", error);
+        });
+});
+
+document.addEventListener('DOMContentLoaded', 
+    function jkl() {
+    fetch('../../components/hero/Hero.html')
+        .then(response => response.text())
+        .then(data => {
+        
+            document.querySelector('.hero-item').innerHTML = data;
         })
         .catch(error => {
             console.error("Error loading navbar:", error);
