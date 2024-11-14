295a14913fe4d68a481055f0cc2b7d46f490ff82
Pawan <pawanchaukiyal@gmail.com>
2024-11-14 14:49:50 +0530

    oK sUCCESS

--- pages/Servicepage/service-info.js
+++ pages/Servicepage/service-info.js
@@ -1,25 +1,30 @@
 const dataPaths = {
-  washing: '../../Data/item_eng.js',
-  fridge: '../../Data/item_eng.js',
-  electrician: '../../Data/extra_eng.js',
-  labour: '../../Data/labour.js'
+  washing: '../../Data/item_eng.json',
+  fridge: '../../Data/item_eng.json',
+  electrician: '../../Data/extra_eng.json',
+  labour: '../../Data/labour.json'
 };
 
 
 const urlParams = new URLSearchParams(window.location.search);
 const value = urlParams.get('value');
+// console.log(value);
 
 
 if (value && dataPaths[value]) {
   fetchDataAndDisplay(value);
+  // console.log("success")
 } else {
   document.getElementById('Sevice-here').innerHTML = '<p>Value not found.</p>';
+  // console.log("failure")
 }
 
 async function fetchDataAndDisplay(value) {
   try {
       const response = await fetch(dataPaths[value]);
+      // console.log(response);
       const data = await response.json();
+      // console.log(data);
 
       let content = `<h2>${value.charAt(0).toUpperCase() + value.slice(1)} Data</h2>`;
       // console.log(content);
@@ -30,11 +35,12 @@ async function fetchDataAndDisplay(value) {
               <div class="card">
                   <img src="${item.image}" alt="${item.name}">
                   <h3>${item.name}</h3>
-                  <p>${item.description}</p>
-                  <p>Price: $${item.price}</p>
-                  <button onclick="window.location.href='../../pages/order/order.html?name=${encodeURIComponent(item.name)}&image=${encodeURIComponent(item.image)}&description=${encodeURIComponent(item.description)}&price=${encodeURIComponent(item.price)}'">Order Now</button>
+                  <p>${item.status}</p>
+                  <p>ById: $${item.job_id}</p>
+                  <button onclick="window.location.href='../BookNow/book.html?name=${encodeURIComponent(item.name)}&image=${encodeURIComponent(item.image)}&status=${encodeURIComponent(item.status)}&job_id=${encodeURIComponent(item.job_id)}'">BookNow</button>
               </div>
           `;
+          // console.log(content)
       });
 
       document.getElementById('Sevice-here').innerHTML = content;
